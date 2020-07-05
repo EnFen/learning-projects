616931a8311e600ba6a4a345b3a0c6ce7d5b54d9
EnFen <nathan.fenimore@gmail.com>
2020-07-05 16:45:32 +1000

    added learnWebpack project

---
+++ learnWebpack/webpack_app/src/js/people.js
@@ -0,0 +1,15 @@
+// module.exports = 'Hello Everyone!';
+
+// function getGreeting() {
+//     return 'Hello There!'
+// }
+// module.exports = getGreeting();
+
+let people = [
+    { name: 'John Doe' },
+    { name: 'Steve Smith' },
+    { name: 'Carol Williams' },
+
+]
+
+module.exports = people;
